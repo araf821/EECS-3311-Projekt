7563aadf600259a8d5b3b1728ba1b0c0cb86ea43
mixhael11 <mangelomasangkay@gmail.com>
2024-03-20 18:15:55 -0400

    removed more verification

--- src/main/java/objects/SystemManagerUser.java
+++ src/main/java/objects/SystemManagerUser.java
@@ -2,15 +2,10 @@ package objects;
 
 public class SystemManagerUser extends User {
     private int employeenumber;
-    private String secret;
 
     public SystemManagerUser(int id, String email, String password, UserType userType, int employeenumber) {
         super(id, email, password, userType);
         this.employeenumber = employeenumber;
-    }
-
-    public String getAnswer() {
-        return secret;
     }
 
 }
