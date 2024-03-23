cdd24b9e9b82bc91ea644e0f0250a1b42cb11394
Mathew-Y <mathewyounan@gmail.com>
2024-03-22 21:08:42 -0400

    Create items in system manager interface

--- src/main/java/org/example/eecs3311project/Main.java
+++ src/main/java/org/example/eecs3311project/Main.java
@@ -2,6 +2,7 @@ package org.example.eecs3311project;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,6 +20,7 @@ public class Main extends Application {
   private CreateManagerAccountController createManagerAccountController;
   private DashboardController dashboardController;
   private SearchController searchController;
+  private AddItemController addItemController;
 
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -98,6 +100,25 @@ public class Main extends Application {
     primaryStage.setTitle("Create Manager Account");
   }
 
+  public void switchToAddItemPage() throws IOException {
+    FXMLLoader addItemLoader = new FXMLLoader(
+        getClass().getResource("fxml/add-item.fxml"));
+    Parent addItemRoot = addItemLoader.load();
+    addItemController = addItemLoader.getController();
+    addItemController.setMain(this); // Pass a reference
+
+    // Create the scene for the create account page
+    Scene addItemScene = new Scene(addItemRoot, 400, 500);
+    addItemScene
+        .getStylesheets()
+        .add(
+            getClass()
+                .getResource("/org/example/eecs3311project/globals.css")
+                .toExternalForm());
+    primaryStage.setScene(addItemScene);
+    primaryStage.setTitle("Add Item");
+  }
+
   public void openDashboardScreen() throws IOException {
     FXMLLoader dashboardLoader = new FXMLLoader(
         getClass().getResource("fxml/dashboard.fxml"));
@@ -118,11 +139,32 @@ public class Main extends Application {
     primaryStage.setScene(dashboardScene);
     primaryStage.setTitle("Dashboard");
   }
-  public void openSearch() throws IOException{
-    
+
+  public void openManagerDashboardScreen() throws IOException {
+    FXMLLoader managerDashboardLoader = new FXMLLoader(
+        getClass().getResource("fxml/manager-dashboard.fxml"));
+    Parent managerDashboardRoot = managerDashboardLoader.load();
+    ManagerDashboardController managerDashboardController = managerDashboardLoader.getController();
+    managerDashboardController.setMain(this); // Pass a reference to the main application to the controller
+
+    // Create the scene for the create account page
+    Scene managerDashboardScene = new Scene(managerDashboardRoot, 800, 450);
+    managerDashboardScene
+        .getStylesheets()
+        .add(
+            getClass()
+                .getResource("/org/example/eecs3311project/globals.css")
+                .toExternalForm());
+
+    // Set the scene on the stage
+    primaryStage.setScene(managerDashboardScene);
+    primaryStage.setTitle("Manager Dashboard");
+  }
+
+  public void openSearch() throws IOException {
+
     FXMLLoader searchloader = new FXMLLoader(
-    getClass().getResource("fxml/searchpage.fxml")
-    );
+        getClass().getResource("fxml/searchpage.fxml"));
     Parent searchRoot = searchloader.load();
     searchController = searchloader.getController();
     searchController.setMain(this);
@@ -130,18 +172,16 @@ public class Main extends Application {
 
     Scene searchscene = new Scene(searchRoot, 800, 450);
     searchscene
-    .getStylesheets()
-    .add(
-      getClass()
-        .getResource("/org/example/eecs3311project/globals.css")
-        .toExternalForm()
-    );
+        .getStylesheets()
+        .add(
+            getClass()
+                .getResource("/org/example/eecs3311project/globals.css")
+                .toExternalForm());
     primaryStage.setScene(searchscene);
     primaryStage.setTitle("Search");
-    
-
 
   }
+
   public static void main(String[] args) {
     launch();
   }
