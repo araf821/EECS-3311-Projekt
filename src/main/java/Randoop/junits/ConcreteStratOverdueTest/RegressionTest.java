959158692232aaf96006cbcba5c3ec21e46a2543
araf821 <araf.ahmed200@gmail.com>
2024-08-10 02:48:55 -0400

    refactor: gitignore useless files

--- src/main/java/Randoop/junits/ConcreteStratOverdueTest/RegressionTest.java
+++
@@ -1,9 +0,0 @@
-package Randoop.junits.ConcreteStratOverdueTest;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ RegressionTest0.class })
-public class RegressionTest {
-}
-
--- src/main/java/Randoop/junits/Faculty/RegressionTest.java
+++
@@ -1,9 +0,0 @@
-package Randoop.junits.Faculty;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ RegressionTest0.class })
-public class RegressionTest {
-}
-
